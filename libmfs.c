0cabb6ece56e61d49a0bc1d202fc0aaaf3547627
urmalhotra <uday@royal-18.cs.wisc.edu>
2022-12-11 21:32:03 -0600

    some server bs + init changes

--- libmfs.c
+++ libmfs.c
@@ -6,10 +6,22 @@ int sd;
 
 int MFS_Init(char *hostname, int port) {
     printf("MFS Init2 %s %d\n", hostname, port);
-    // do some net setup
     sd = UDP_Open(20000);
-    int rc = UDP_FillSockAddr(&addrSnd, hostname, port);
-    return rc;
+    assert(sd > -1);
+    UDP_FillSockAddr(&addrSnd, hostname, port);
+    //tell server to open port?
+    struct message_t to_send;
+    to_send.mtype = 1;
+    //filler for message
+    to_send.message[0] = port;
+    to_send.message[sizeof(port)] = '\0';
+    int rc_send = UDP_Write(sd, &addrSnd, to_send, 1024);
+    if (rc < 0) {
+        printf("client:: failed to send\n");
+        exit(1);
+    }
+    UDP_Read(sd, &addrRcv, message, 1024);
+    return to_send.rc;
 }
 
 int MFS_Lookup(int pinum, char *name) {
@@ -27,7 +39,7 @@ int MFS_Lookup(int pinum, char *name) {
         exit(1);
     }
     //implement timeout
-    to_send.rc = UDP_Read(sd, &addrRcv, message, 1024);
+    UDP_Read(sd, &addrRcv, message, 1024);
     if (to_send.rc < 0){
         return rc;
     }
--- server.c
+++ server.c
@@ -3,23 +3,48 @@
 
 #include "message.h"
 
-// server code
-int main(int argc, char *argv[]) {
-    int sd = UDP_Open(10000);
-    assert(sd > -1);
-    while (1) {
-	struct sockaddr_in addr;
+int sd;
+struct sockaddr_in addr_receive, sockaddr_in addr_out;
 
-	message_t m;
-	
-	printf("server:: waiting...\n");
-	int rc = UDP_Read(sd, &addr, (char *) &m, sizeof(message_t));
-	printf("server:: read message [size:%d contents:(%d)]\n", rc, m.mtype);
-	if (rc > 0) {
-	    m.rc = 3;
-            rc = UDP_Write(sd, &addr, (char *) &m, sizeof(message_t));
-	    printf("server:: reply\n");
-	} 
+void intHandler(int dummy) {
+    UDP_Close(sd);
+    exit(130);
+}
+
+int main(int argc, char *argv[]) {
+	signal(SIGINT, intHandler);
+	sd = UDP_Open(10000);
+	assert(sd > -1);
+	//opening file image 
+	char img_path[1024];
+	if (argv[2] == '0'){
+		//incomplete args
+		return -1;
+	}
+	else{
+		img_path = argv[2];
+	}
+	int fd = open(img, O_CREAT | O_RDWR | O_EXCL, 0644);
+	struct stat st;
+	if(fstat(fd, &st) == -1){
+		perror("error can't read size");
+	};
+	//mapping image to file
+	char *file_ptr = mmap(NULL, st.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+	// loop through, read requests, call appropriate function...
+	while (1) {
+		int to_do;
+		message_t m;
+		printf("server:: waiting...\n");
+		int rc = UDP_Read(sd, &addr_receive, (char *) &m, sizeof(message_t));
+		printf("server:: read message [size:%d contents:(%d)]\n", rc, m.mtype);
+		if (rc > 0) {
+			to_do = m.mtype;
+			if (mtype == 0){
+				//call init
+			}
+		} 
     }
     return 0; 
 }
